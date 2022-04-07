38368012c39c170dff5b8d8cf4b2ce767afe961a
CodingWithRoyal <asktojatt@gmail.com>
2022-04-07 07:50:28 +0530

    Done

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,7 +1,9 @@
 import re
 
+def arithmetic_arranger(problems, solveIt=False):
 
-def arithmetic_arranger(problems):
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
     resp = ""
     l1 = ""
@@ -9,17 +11,37 @@ def arithmetic_arranger(problems):
     l3 = ""
     l4 = ""
 
-    for p in problems:
-        p = p.split()  # problem
-        length = len(max(p))  # length of biggest operand in list
+    for prob in problems:
+        p = prob.split()  # problem
+        try:
+            p[0] = int(p[0])
+            p[2] = int(p[2])
+        except Exception:
+            return "Error: Numbers must only contain digits."
+        try:
+            p[1] = str(p[1])
+        except Exception:
+            return "Error: Operator must be '+' or '-'."
+        
+        # largest operand
+        lOperand = p[2]
+        if p[0] > p[2]:
+            lOperand = p[0]
+        length = len( str(lOperand) )  # length of biggest operand in list
+
+        if length > 4:
+            return "Error: Numbers cannot be more than four digits."
 
         # There should be a single space between the operator and the longest of the two operands, the operator will be on the same line as the second operand, both operands will be in the same order as provided (the first will be the top one and the second will be the bottom.
         probSpace = length + 1 + 1
 
+        # line 1
+        # line 2
         i = 0  # index
         for x in p:
+            x = str(x)
             if i != 1:
-                spacesToAdd = probSpace - len(x)
+                spacesToAdd = probSpace - len( x )
                 if i == 2:
                     spacesToAdd -= 1
 
@@ -38,15 +60,44 @@ def arithmetic_arranger(problems):
                 else:
                     l2 += x + "    "
 
-            # line 3 - dash
-            dashesToAdd = probSpace
-            while dashesToAdd > 0:
-                l3 += "-"
-                dashesToAdd -= 1
-            l3 += "    "
-
             # increase index
             i += 1
 
-    resp = l1 + "\n" + l2 + "\n" + l3
+        # line 3 - dash
+        dashesToAdd = probSpace
+        while dashesToAdd > 0:
+            l3 += "-"
+            dashesToAdd -= 1
+        l3 += "    "
+
+        # line 4 - result
+        x = p[0]
+        y = p[2]
+        operator = p[1]
+
+        if operator == "+":
+            result = x+y
+        elif operator == "-":
+            result = x-y
+        else:
+            return "Error: Operator must be '+' or '-'."
+        
+        result = str( result )
+        spacesToAdd = probSpace - len(result)
+        # make it right align
+        while spacesToAdd > 0:
+            result = ' '+result
+            spacesToAdd -= 1
+        l4 += result+"    "
+
+    l1 = l1.rstrip()
+    l2 = l2.rstrip()
+    l3 = l3.rstrip()
+    l4 = l4.rstrip()
+
+    if solveIt == True:
+        resp = l1 + "\n" + l2 + "\n" + l3 + "\n" + l4
+    else:
+        resp = l1 + "\n" + l2 + "\n" + l3
+
     return resp
